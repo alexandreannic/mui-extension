678d4005119bb36da4fbc5659b114f2ebf442baf
Alexandre Annic <alexandre.annic1@gmail.com>
2021-06-10 14:27:52 +0200

    Btn: handle error color according to variant

--- src/Btn/Btn.tsx
+++ src/Btn/Btn.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {Button, CircularProgress, createStyles, Icon, makeStyles, Theme} from '@material-ui/core'
+import {Button, CircularProgress, createStyles, fade, Icon, makeStyles, Theme} from '@material-ui/core'
 import classNames from 'classnames'
 import {ButtonProps} from '@material-ui/core/Button'
 import {colorError} from '../core/style/color'
@@ -29,7 +29,14 @@ const useStyles = makeStyles((t: Theme) => createStyles({
   labelHidden: {
     visibility: 'hidden',
   },
-  error: {
+  error_text: {
+    color: colorError,
+  },
+  error_outlined: {
+    color: colorError,
+    borderColor: fade(colorError, .4),
+  },
+  error_contained: {
     background: colorError,
     color: 'white',
   },
@@ -48,7 +55,7 @@ export const Btn = ({loading, children, disabled, icon, iconAfter, color, ...pro
     <Button
       {...props}
       color={color === 'error' ? 'default' : color}
-      className={classNames(color === 'error' && classes.error)}
+      className={classNames(color === 'error' && (classes['error_' + props.variant]))}
       disabled={disabled || loading}
     >
       <div className={classNames(classes.label, loading && classes.labelHidden)}>
